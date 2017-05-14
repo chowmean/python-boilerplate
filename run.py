626f5f558fe790bd2194e951c524c1967b449d4c
chowmean <gaurav.dev.iiitm@gmail.com>
2017-05-14 22:14:19 +0530

    adding beanstalk config

--- run.py
+++ run.py
@@ -1,8 +1,11 @@
 from config import config
 from mysqlconn import conn
 from logger import logger, sentry
+from bstalk import beanstalk
 if conn:
 	print "Connection Succesfull"
+
 print logger
 print sentry
+print beanstalk
 
