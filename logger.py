0be4f3ffc5dc7dc5d229d1b2c325d82a82a0bf0e
chowmean <gaurav.dev.iiitm@gmail.com>
2017-05-14 22:05:10 +0530

    added sentry

--- logger.py
+++ logger.py
@@ -1,10 +1,12 @@
 import logging
 import graypy
 from config import config
+from raven import Client
 
 logger = logging.getLogger(config.get('LOGGER','name'))
 logger.setLevel(logging.DEBUG)
-
 handler = graypy.GELFHandler(config.get('LOGGER','host'),
 							 config.get('LOGGER','port'))
 logger.addHandler(handler)
+
+sentry = Client(config.get('SENTRY','uri'))
--- run.py
+++ run.py
@@ -1,5 +1,8 @@
 from config import config
 from mysqlconn import conn
+from logger import logger, sentry
 if conn:
 	print "Connection Succesfull"
+print logger
+print sentry
 
