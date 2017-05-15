0e713184101b023c6aabbe8eccbb55cbf4bb6d3b
chowmean <gaurav.dev.iiitm@gmail.com>
2017-05-16 00:32:55 +0530

    adding redis

--- run.py
+++ run.py
@@ -2,11 +2,13 @@ from config import config
 from mysqlconn import conn
 from logger import logger, sentry
 from bstalk import beanstalk
+from redisconn import REDIS_CLI
 if conn:
 	print "Connection Succesfull"
 
 print logger
 print sentry
 print beanstalk
+print REDIS_CLI
 
 print "testing travis"
